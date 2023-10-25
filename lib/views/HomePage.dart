eac286e62d0fcf883ca562ba42ef81bdb2fe3843
Hagarabdelhamed <83194017+Hagarabdelhamed@users.noreply.github.com>
2023-10-26 00:58:22 +0300

    AppBar

---
+++ lib/views/HomePage.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Center(
+          child: RichText(
+            text: const TextSpan(
+              text: 'News',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 23,
+                fontWeight: FontWeight.bold,
+              ),
+              children: <TextSpan>[
+                TextSpan(
+                  text: 'Cloud',
+                  style: TextStyle(
+                    color: Color(0xffFFB000),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/views/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/views/HomePage.dart';
+
+void main() {
+  runApp(const NewsApp());
+}
+
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ); // This trailing comma makes auto-formatting nicer for build methods.
+   
+  }
+}
